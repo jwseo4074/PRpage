0e45d6ce87ccd8f0949d4afa49f95404f689fc8c
zwon2056 <zwon2056@gmail.com>
2022-04-26 04:50:51 +0900

    Test Commit ver3

--- myapp/src/page/Footer.js
+++ myapp/src/page/Footer.js
@@ -4,6 +4,7 @@ function Footer() {
             <div className="footer">
                 <h1>Footer 입니다아아아아.</h1>
                 <h2>Test Commit</h2>
+                <h3>Test Commit</h3>
             </div>
         </>
     );
