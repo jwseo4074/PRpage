bdeccf60e1fe3d34eae6287585657d41ffd2cb67
zwon2056 <zwon2056@gmail.com>
2022-05-30 15:50:28 +0900

    Refactoring 5/30 15:36

--- myapp/src/article/Project.js
+++ myapp/src/article/Project.js
@@ -1,71 +1,102 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 // import Fade from 'react-reveal/Fade';
-import "../style/Article.css";
-import "../style/ContentPage.css";
+import '../style/Article.css';
+import '../style/ContentPage.css';
 
 function Project() {
-    return (
-        <>  
-            <a class="moveToTag" name="moveToProject"></a>
-            <div id ="project_header_box">
-                <h1 id="article_title"> Project </h1>
-            </div>
-            <div id = "project_big_box">
-                <div id ="up_box_in_project_big_box">
-                    <div className="project">
-                        <a href="https://github.com/JO2SEO/AOMD" target="_blank">
-                            <div className="number">
-                                <p className="numberCanHover">AOMD</p>
-                                <div className ="explain_and_smallName">
-                                    <span className="explain"> 부산대학교 </span>
-                                    <span className="smallName"> 졸업과제 </span>
-                                </div> 
-                                <p className="explainProject"> 하이퍼레저 패브릭을 이용한 웹 플랫폼 </p>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="project">
-                        <a href="https://qkrtmfqls.gabia.io/#/" target="_blank">
-                            <div className="number">
-                                <p className="numberCanHover">Refri Bank</p>
-                                <div className ="explain_and_smallName">
-                                    <span className="explain"> 부산 연합 SW 동아리 </span>
-                                    <span className="smallName">PROJECT</span> 
-                                </div>
-                                <p className="explainProject"> 6 WEEKS PROJECT </p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-                <div id="bottom_box_in_project_big_box">
-                    <div className="project">
-                        <a href="https://qkrtmfqls.gabia.io/#/" target="_blank">
-                            <div className="number">
-                                <p className="numberCanHover">PR page</p>
-                                <div className ="explain_and_smallName">
-                                    <span className="explain"> 개인 프로젝트 </span>
-                                    <span className="smallName"> React.js</span> 
-                                </div>
-                                <p className="explainProject"> 나만의 PR page 만들기</p>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="project">
-                        <a href="https://todolist-zwon.herokuapp.com" target="_blank">
-                            <div className="number">
-                                <p className="numberCanHover">Todo List</p>
-                                <div className ="explain_and_smallName">
-                                    <span className="explain"> NomadCoder </span>
-                                    <span className="smallName"> lecture </span> 
-                                </div>
-                                <p className="explainProject"> Vanila JS를 이용한 Todo List 만들기 </p>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<a class="moveToTag" name="moveToProject"></a>
+			<div id="project_header_box">
+				<h1 id="article_title"> Project </h1>
+			</div>
+			<div id="project_big_box">
+				<div id="up_box_in_project_big_box">
+					<div className="project">
+						<a href="https://github.com/JO2SEO/AOMD" target="_blank">
+							<div className="number">
+								<p className="numberCanHover">AOMD</p>
+								<div className="explain_and_smallName">
+									<span className="explain"> 부산대학교 </span>
+									<span className="smallName"> 졸업과제 </span>
+								</div>
+								<p className="explainProject">
+									하이퍼레저 패브릭을 이용한 웹 플랫폼 설계
+								</p>
+							</div>
+						</a>
+					</div>
+					<div className="project">
+						<a href="https://qkrtmfqls.gabia.io/#/" target="_blank">
+							<div className="number">
+								<p className="numberCanHover">Refri Bank</p>
+								<div className="explain_and_smallName">
+									<span className="explain"> 부산 연합 SW 동아리 </span>
+									<span className="smallName">PROJECT</span>
+								</div>
+								<p className="explainProject">6 WEEKS PROJECT</p>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div id="bottom_box_in_project_big_box">
+					<div className="project">
+						<a href="https://qkrtmfqls.gabia.io/#/" target="_blank">
+							<div className="number">
+								<p className="numberCanHover"> PR page </p>
+								<div className="explain_and_smallName">
+									<span className="explain"> Toy Project </span>
+									<span className="smallName"> Publishing</span>
+								</div>
+								<p className="explainProject"> zwon's PR page using JS, CSS</p>
+							</div>
+						</a>
+					</div>
+					<div className="project">
+						<a href="https://todolist-zwon.herokuapp.com" target="_blank">
+							<div className="number">
+								<p className="numberCanHover">Todo List </p>
+								<div className="explain_and_smallName">
+									<span className="explain"> Toy Project </span>
+									<span className="smallName"> Vanila JS </span>
+								</div>
+								<p className="explainProject"> todoList using Vanila JS </p>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div id="bottom_box_in_project_big_box">
+					<div className="project">
+						<a href="https://qkrtmfqls.gabia.io/#/" target="_blank">
+							<div className="number">
+								<p className="numberCanHover"> movieWeb </p>
+								<div className="explain_and_smallName">
+									<span className="explain"> Toy Project </span>
+									<span className="smallName"> React.js</span>
+								</div>
+								<p className="explainProject">
+									{' '}
+									공부해왔던 모든 것을 접목시킨 웹 사이트{' '}
+								</p>
+							</div>
+						</a>
+					</div>
+					<div className="project">
+						<a href="https://todolist-zwon.herokuapp.com" target="_blank">
+							<div className="number">
+								<p className="numberCanHover"></p>
+								<div className="explain_and_smallName">
+									<span className="explain"> </span>
+									<span className="smallName"> </span>
+								</div>
+								<p className="explainProject"></p>
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Project;
